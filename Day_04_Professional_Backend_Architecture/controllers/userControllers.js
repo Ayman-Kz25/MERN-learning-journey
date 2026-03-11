0032e7a58ef80d68d756f7e35d07138040d62d4f
Ayman-Kz25 <kamalakz786@gmail.com>
2026-03-11 13:36:48 +0500

    day 04 practice work tested

--- Day_04_Professional_Backend_Architecture/controllers/userControllers.js
+++ Day_04_Professional_Backend_Architecture/controllers/userControllers.js
@@ -1,36 +1,36 @@
 let users = [];
 
 exports.getUsers = (req, res) => {
-    res.send(users);
+  res.send(users);
 };
 
 exports.createUser = (req, res) => {
-    const user = req.body;
+  const user = req.body;
 
-    users.push(user);
+  users.push(user);
 
-    res.send({
-        message: "User created",
-        data: user
-    });
+  res.send({
+    message: "User created",
+    data: user,
+  });
 };
 
 exports.getUserById = (req, res) => {
-    const id = Number(req.params.id);
+  const id = Number(req.params.id);
 
-    const user = users.find(u => u.id === id);
+  const user = users.find((u) => u.id === id);
 
-    if(!user) {
-        return res.send("User not found!");
-    }
+  if (!user) {
+    return res.send("User not found!");
+  }
 
-    res.send("User found: ", user);
+  res.send(user);
 };
 
-exports.deleteuser = (req, res) => {
-    const id = Number(req.params.id);
+exports.deleteUser = (req, res) => {
+  const id = Number(req.params.id);
 
-    users = users.filter(u => u.id !== id);
+  users = users.filter((u) => u.id !== id);
 
-    res.send(`User ${id} deleted!`);
+  res.send(`User ${id} deleted!`);
 };
