a40bfd47fb9aa14c223cb07ea8019d064f0eff1d
Ayman-Kz25 <kamalakz786@gmail.com>
2026-03-11 13:18:39 +0500

    day 04 controller file created

--- Day_04_Professional_Backend_Architecture/controllers/userControllers.js
+++ Day_04_Professional_Backend_Architecture/controllers/userControllers.js
@@ -1,5 +1,36 @@
 let users = [];
 
 exports.getUsers = (req, res) => {
-    
-}
+    res.send(users);
+};
+
+exports.createUser = (req, res) => {
+    const user = req.body;
+
+    users.push(user);
+
+    res.send({
+        message: "User created",
+        data: user
+    });
+};
+
+exports.getUserById = (req, res) => {
+    const id = Number(req.params.id);
+
+    const user = users.find(u => u.id === id);
+
+    if(!user) {
+        return res.send("User not found!");
+    }
+
+    res.send("User found: ", user);
+};
+
+exports.deleteuser = (req, res) => {
+    const id = Number(req.params.id);
+
+    users = users.filter(u => u.id !== id);
+
+    res.send(`User ${id} deleted!`);
+};
