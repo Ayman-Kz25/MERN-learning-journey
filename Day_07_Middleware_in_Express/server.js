17f7851185dde2e23f9f3d79c01d976de380b2a9
Ayman-Kz25 <kamalakz786@gmail.com>
2026-03-24 13:30:48 +0500

    day 7 folder and files created

---
+++ Day_07_Middleware_in_Express/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const productRoute = require("./routes/productRoute");
+
+const app = express();
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB Connected!"))
+  .catch((err) => console.log("MongoDB Connection Error: ",err));
+
+app.use("/products", productRoute);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
